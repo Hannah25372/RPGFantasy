09dea055c61286e8d21c6ee442552afa7ec2122e
Hannah25372 <127040735+Hannah25372@users.noreply.github.com>
2024-02-20 14:56:07 +0000

    some sifting ideas

--- Y3ProjectTest/Assets/Scripts/mainPlayer.cs
+++ Y3ProjectTest/Assets/Scripts/mainPlayer.cs
@@ -3,20 +3,54 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class mainPlayer : MonoBehaviour
+
+    
 {
+
+    private bool menu;
+    private GameObject menuImage;
+    private npcScript lastNPC;
+
+
     // Start is called before the first frame update
     void Start()
     {
         CursorOff();
-
-
-
+        menu = false;
+        menuImage = GameObject.Find("Canvas").transform.Find("KeyMap").gameObject;
+        menuImage.SetActive(false);
+        lastNPC = null;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        if (menu)
+        {
+            menuImage.SetActive(true);
+            if (Input.GetKey(KeyCode.F))
+            {
+                Debug.Log("Pressed F");
+                simulation.AddToLog("fight", "0", lastNPC.IDController.ToString());
+            }
+            else if (Input.GetKey(KeyCode.G))
+            {
+                simulation.AddToLog("give", "0", lastNPC.IDController.ToString());
+            }
+            else if (Input.GetKey(KeyCode.R))
+            {
+                simulation.AddToLog("steal", "0", lastNPC.IDController.ToString());
+            }
+            else if (Input.GetKey(KeyCode.T))
+            {
+                simulation.AddToLog("talk", "0", lastNPC.IDController.ToString());
+            }
+        } else
+        {
+            menuImage.SetActive(false);
+        }
+ 
     }
 
 
@@ -27,10 +61,23 @@ public class mainPlayer : MonoBehaviour
         {
             case "NPC":
                 Debug.Log("Player crashed into NPC");
-
+                menu = true;
                 //bring up menu for talk or fight? can have it as f to fight and t to talk. g to give and r to steal
+                lastNPC = collision.gameObject.GetComponent<npcScript>();
 
+                break;
+        }
+    }
 
+    private void OnCollisionExit(Collision collision)
+    {
+
+        switch (collision.gameObject.tag)
+        {
+            case "NPC":
+                Debug.Log("Player left into NPC");
+                menu = false;
+                lastNPC = null;
                 break;
         }
     }
--- Y3ProjectTest/Assets/Scripts/simulation.cs
+++ Y3ProjectTest/Assets/Scripts/simulation.cs
@@ -6,6 +6,10 @@ public class simulation : MonoBehaviour
 {
 
     public static List<string> log;
+    public static int lastViewedLog;
+    public static string currentLog;
+    private List<Pattern> patterns;
+    
 
     //public List<npcScript> characters;
     //they have: HP, AC, ATK, traits, relationships, objectsHolding, location
@@ -77,7 +81,7 @@ public class simulation : MonoBehaviour
             }
             else if (traits1.Contains(Traits.Aggressive) && traits2.Contains(Traits.Aggressive))
             {
-                state1 = State.FIGHTING; 
+                state1 = State.FIGHTING;
                 state2 = State.FIGHTING;
             }
             else if (traits1.Contains(Traits.Selfish))
@@ -128,10 +132,25 @@ public class simulation : MonoBehaviour
         return state;
     }
 
+
+
     // Update is called once per frame
     void Update()
     {
-        
+       
+        if (lastViewedLog < log.Count - 1) //length = 3. last viewed is 2
+        {
+            lastViewedLog++;
+            currentLog = log[lastViewedLog];
+            string[] currentLogBreakdown = currentLog.Split(".");
+
+            
+            //does this log complete /advance any partial patterns?
+
+            //does this log start a pattern?
+
+            //what events will be needed to advance any partial patterns? give suggestions
+        }
 
 
     }
@@ -169,4 +188,92 @@ public class simulation : MonoBehaviour
     }
 
 
+    private void SetUpPatterns()
+    {
+        //revenge pattern
+        List<Event> events1 = new List<Event>();
+        events1.Add(new Event("fight", "A", "B", null, null));
+        events1.Add(new Event("escape", "B", "A", null, null));
+        events1.Add(new Event("fight", "B", "A", null, null));
+        List<Event> nullEvents1 = new List<Event>();
+        nullEvents1.Add(new Event("kill", null, "B", null, null));
+        nullEvents1.Add(new Event("kill", null, "A", null, null));
+        Pattern pattern1 = new(PatternName.revenge, events1, nullEvents1);
+
+    }
+
 }
+
+
+
+
+//stores an individual event, with the parts that make up an event
+class Event
+{
+    string action;
+    string char1;
+    string char2;
+    string obj;
+    string loc; 
+
+    public Event(string action, string char1, string char2, string obj, string loc)
+    {
+        this.action = action;
+        this.char1 = char1;
+        this.char2 = char2;
+        this.obj = obj;
+        this.loc = loc;
+    }
+}
+
+
+//stores a story pattern, including the events and the nullifying events for a pattern
+//the default character letter used denotes where conditions have to be met of the same character being used etc.
+class Pattern {
+
+    PatternName name;
+    List<Event> events;
+    List<Event> nullifyEvents;
+
+    public Pattern(PatternName name, List<Event> events, List<Event> nullifyEvents)
+    {
+        this.name = name;
+        this.events = events;
+        this.nullifyEvents = nullifyEvents;
+    }
+
+    public Event getEvent(int no)
+    {
+        return events[no];
+    }
+    public int noEvents()
+    {
+        return events.Count;
+    }
+
+}
+
+//holds the pattern being followed, where you are up to, which characters, objects end up getting used
+class PartialPatternBlock
+{
+    Pattern pattern; //add the pattern in question
+    int nextEvent;
+    string char1;
+    string char2;
+    string char3; //can end up having patterns with 3 characters (but an individual events would only have max 2 characters)
+    string obj;
+    string loc;
+
+    public Event getNextEvent()
+    {
+       return pattern.getEvent(nextEvent);      
+    }
+
+}
+
+//pattern names
+enum PatternName
+{
+    revenge, betrayal
+}
+
